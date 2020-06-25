30b3241190ea2efe52b88143750dece39a5792a6
Letícia <leticiatakeshiro@hotmail.com>
2020-06-24 21:27:10 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="corpo">
+        <meu-menu :rotas="routes" />
+        <transition name="pagina">
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
+
+<script>
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
+
+export default {
+    components: {
+        'meu-menu': Menu
+    },
+    data() {
+        return {
+            routes
+        };
+    }
+};
+</script>
+
+<style>
+.corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+}
+
+.pagina-enter,
+.pagina-leave-active {
+    opacity: 0;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+    transition: opacity 0.4s;
+}
+</style>
---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav>
+        <ul>
+            <li v-for="rota in rotas">
+                <router-link :to="rota.path ? rota.path : '/'">{{rota.titulo}}</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        rotas: {
+            required: true,
+            type: Array
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
